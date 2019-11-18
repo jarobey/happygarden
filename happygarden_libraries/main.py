fdb1f6484e2fa61332546a9c061a6e90e7cfbb09
Jason Robey <jason@MiddleEarth.local>
2019-11-18 11:25:46 -0800

    Removed commented IP--turned out space in device name is not supported by AHK

--- happygarden_libraries/main.py
+++ happygarden_libraries/main.py
@@ -1,5 +1,5 @@
 from happygarden.ahk import GardenHub
 import socket
 
-hub = GardenHub('IMACBridge','pi','10.0.10.200')#, listen_address='10.0.10.26')
+hub = GardenHub('IMACBridge','pi','10.0.10.200'))
 hub.start()
